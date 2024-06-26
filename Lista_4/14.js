a4d6c132361d9eb2eac88616055b8096c3742152
João Brocca <162700492+JoaoPedroB08@users.noreply.github.com>
2024-06-26 11:36:58 -0300

    Update 14.js

--- Lista_4/14.js
+++ Lista_4/14.js
@@ -3,3 +3,25 @@
 // a) Ter pelo menos 65 anos
 // b) Ter trabalhado pelo menos 30 anos
 // c) Ter pelo menos 60 anos e ter trabalhado pelo menos 25 anos
+
+
+idadeUsuario = Number(prompt('Digite sua idade: '))
+anosTrabalhados =Number(prompt('Digite quantos anos você está no seu trabalho: '))
+
+if(idadeUsuario >= 65){
+
+    alert('Você já pode se aposentar!')
+
+}else if(idadeUsuario >= 60 & anosTrabalhados >= 25){
+
+    alert('Você já pode se aposentar!')
+
+}else if(anosTrabalhados >= 30){
+
+    alert('Você já pode se aposentar!')
+
+}else{
+
+    alert('Pelos seus dados, você ainda não pode se aponter...')
+
+}
