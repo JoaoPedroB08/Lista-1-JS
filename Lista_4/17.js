9adf373276036f1b798585f43724d5b7d6b6a72d
João Brocca <162700492+JoaoPedroB08@users.noreply.github.com>
2024-06-26 21:37:46 -0300

    Update 17.js

--- Lista_4/17.js
+++ Lista_4/17.js
@@ -4,3 +4,33 @@
 //    LARANJA - LARANJA - LARANJA -> R$80
 //    CEREJA - LARANJA - CEREJA -> R$50
 //    LARANJA - CEREJA - LARANJA -> R$25
+
+fruta1 = prompt('Digite qual fruta foi sorteada primeiro (Laranja ou Cereja): ').toLocaleLowerCase()
+fruta2 = prompt('Digite qual fruta foi sorteada de segunda (Laranja ou Cereja): ').toLocaleLowerCase()
+fruta3 = prompt('Digite qual fruta foi sorteada na terceira (Laranja ou Cereja): ').toLocaleLowerCase()
+
+if(fruta1 == 'cereja' & fruta2 == 'cereja' & fruta3 == 'cereja'){
+
+    alert('JACKPOT!!! você ganhou R$100')
+
+}else if(fruta1 == 'laranja' & fruta2 == 'laranja' & fruta3 == 'laranja'){
+
+    alert('Quase! você ganhou R$80')
+
+}else if(fruta1 == 'cereja' & fruta2 == 'laranja' & fruta3 == 'cereja'){
+
+    alert('Boa tentativa! você ganhou R$50')
+
+}else if(fruta1 == 'laranja' & fruta2 == 'cereja' & fruta3 == 'laranja'){
+
+    alert('você ganhou R$25, mais sorte na próxima!')
+
+}else if(fruta1 != fruta3){
+
+    alert('Infelizmente não foi dessa vez, mas ainda há tempo, nunca é hora de desistir!')
+
+}else{
+
+    alert('Erro, tente digitar novamente as frutas solicitadas!')
+
+}
